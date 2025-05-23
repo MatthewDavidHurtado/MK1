b5538178c656916d447df49512f2cd3605df70bb
MALCOLM KINGLEY <71328086+MatthewDavidHurtado@users.noreply.github.com>
2025-05-23 07:32:23 -0700

    Update video source in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 const Header: React.FC = () => {
@@ -11,19 +10,18 @@ const Header: React.FC = () => {
         The Ultimate Shortcut to Wealth and Divine Authority Is Right Here (Watch This Video).
       </p>
 
-      {/* Vimeo Video Embed */}
+      {/* Video Container */}
       <div className="mt-6 mb-4 md:mb-6 max-w-2xl mx-auto">
         <div style={{padding:"57.82% 0 0 0", position:"relative"}}>
-          <iframe 
-            src="https://player.vimeo.com/video/1087098760?h=a856447905&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
-            frameBorder="0" 
-            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" 
-            style={{position:"absolute", top:0, left:0, width:"100%", height:"100%"}} 
-            title="Permanent Overflow Is Yours Already - Claim It."
-            allowFullScreen
-          ></iframe>
+          <video 
+            controls
+            style={{position:"absolute", top:0, left:0, width:"100%", height:"100%"}}
+            preload="metadata"
+          >
+            <source src="https://healvideos.s3.us-east-2.amazonaws.com/permanent_overflow_is_yours_already_-_claim_it.+(720p).mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
-        <script src="https://player.vimeo.com/api/player.js"></script>
       </div>
 
       <div className="mt-4 md:mt-6">
